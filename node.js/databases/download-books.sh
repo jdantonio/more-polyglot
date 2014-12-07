b97882c1dde2805b5a33668ed78ea9801f5abe0a
Jerry D'Antonio <stumpjumper@gmail.com>
2014-12-07 18:18:31 -0500

    Updated Node.js database download/import script

--- node.js/databases/download-books.sh
+++ node.js/databases/download-books.sh
@@ -12,4 +12,19 @@ mkdir test
 echo 'Copying the required RDF file...'
 cp cache/epub/132/pg132.rdf test/
 
+#echo 'Creating the book database...'
+#./dbcli.js -m PUT -p books
+
+#echo 'Importing books into the database...'
+#./import-books.js
+
+#$ ./dbcli.js -m GET -p books
+#$ ./dbcli.js -m GET -p books/0
+#200 { _id: '0',
+  #_rev: '1-453265faaa77a714d46bd72f62326186',
+  #title: '',
+  #authors: [],
+  #subjects: [] }
+#$ ./dbcli.js -m DELETE -p books/0?rev=1-453265faaa77a714d46bd72f62326186
+
 echo 'Done'
