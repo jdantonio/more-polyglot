a34a686369c2ca4f3c167cd3a2ee086118220ac0
Jerry D'Antonio <stumpjumper@gmail.com>
2014-12-08 22:59:21 -0500

    Passing Ruby Warrior Level 4, needs refactoring.

--- ruby-warrior/rubywarrior/stumpjumper-beginner/player.rb
+++ ruby-warrior/rubywarrior/stumpjumper-beginner/player.rb
@@ -1,43 +1,65 @@
-class InitialGameState
+UnexpectedGameStateError = Class.new(StandardError)
 
-  attr_reader :warrior_max_health
+class GameState
+
+  attr_reader :warrior_health
   
   def initialize(warrior)
-    @warrior_max_health = warrior.health
+    @warrior_health = warrior.health
   end
 end
 
 class Player
 
-  UnexpectedWarriorStateError = Class.new(StandardError)
-
-  def initial_game_state(warrior)
-    @initial_game_state ||= InitialGameState.new(warrior).freeze
-  end
-
   def play_turn(warrior)
     init_state = initial_game_state(warrior)
 
-    if facing_enemy?(warrior)
+    if confronted_by?(:enemy, warrior)
       warrior.attack!
-    elsif taken_damage?(warrior, init_state.warrior_max_health)
-      warrior.rest!
+    elsif taken_damage?(warrior, init_state)
+      if taken_damage?(warrior, last_game_state)
+        warrior.walk!(:forward)
+      else
+        warrior.rest!
+      end
     elsif can_move_forward?(warrior)
       warrior.walk!(:forward)
     else
-      raise UnexpectedWarriorStateError
+      raise UnexpectedGameStateError
     end
+
+    save_current_game_state(warrior)
   end
+
+  ## game state
+
+  attr_reader :last_game_state
+
+  def initial_game_state(warrior)
+    if @initial_game_state.nil?
+      @initial_game_state = GameState.new(warrior).freeze
+      save_current_game_state(warrior)
+    end
+    @initial_game_state
+  end
+
+  def save_current_game_state(warrior)
+    @last_game_state = GameState.new(warrior).freeze
+  end
+
+  ## warrior status
 
   def can_move_forward?(warrior)
     warrior.feel(:forward).empty?
   end
 
-  def facing_enemy?(warrior)
-    warrior.feel(:forward).enemy?
+  def confronted_by?(entity, warrior)
+    warrior.feel(:forward).send("#{entity}?".to_sym)
+  rescue NoMethodError
+    false
   end
 
-  def taken_damage?(warrior, max_health)
-    warrior.health < max_health
+  def taken_damage?(warrior, game_state)
+    warrior.health < game_state.warrior_health
   end
 end
