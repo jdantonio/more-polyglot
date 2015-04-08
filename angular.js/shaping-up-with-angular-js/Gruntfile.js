1637535fb254c0e7918bcba95ee8325827dd8566
Jerry D'Antonio <stumpjumper@gmail.com>
2015-04-07 23:45:31 -0400

    CodeSchool Angular code now built with Grunt and Bower.

--- angular.js/shaping-up-with-angular-js/Gruntfile.js
+++ angular.js/shaping-up-with-angular-js/Gruntfile.js
@@ -1,18 +1,90 @@
 module.exports = function(grunt){
 
-  grunt.config.init({
-    hello: {
-      options: {
-        greeting: 'Hello from Grunt.',
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.config('concat', {
+    scripts: {
+      src: ['bower_components/jquery/dist/jquery.js',
+            'bower_components/angular/angular.js',
+            'app.js'],
+      dest: 'tmp/app.js'
+    },
+    styles: {
+      src: ['bower_components/bootstrap/dist/css/bootstrap.css'],
+      dest: 'tmp/app.css'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.config('uglify', {
+    options: {
+      mangle: true,
+      compress: true
+    },
+    scripts: {
+      files: {
+        'assets/app.js' : 'tmp/app.js'
       }
     }
   });
-  
-  grunt.registerTask('hello', 'Hello World!', function(){
-    this.requiresConfig(this.name + '.options.greeting');
-    var greeting = this.options().greeting;
-    grunt.log.writeln(greeting);
+ 
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.config('cssmin', {
+    app: {
+      files: {
+        'assets/app.css': ['tmp/app.css']
+      }
+    }
   });
 
-  grunt.registerTask('default', 'Run all tasks', ['hello']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.config('copy', {
+    scripts: {
+      files: [
+          {expand: true, cwd: 'tmp', src: 'app.js', dest: 'assets'}
+      ]
+    },
+    styles: {
+      files: [
+          {expand: true, cwd: 'tmp', src: 'app.css', dest: 'assets'}
+      ]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.config('watch', {
+    scripts: {
+      files: ['./**/*.js'],
+      tasks: ['build:scripts'],
+	  
+      options: {
+        spawn: false
+      }
+    },
+    styles: {
+      files: ['./**/*.css'],
+      tasks: ['build:styles'],
+      options: {
+        spawn: false
+      }
+    },
+    //interface: {
+      //files: ['index.html']
+    //},
+    //options: {
+      //livereload: true
+    //}
+  }); 
+
+  grunt.registerTask('build:scripts', "Builds the javascript.",
+                    ['concat:scripts', 'copy:scripts']);
+                    //['concat:scripts', 'uglify']);
+
+  grunt.registerTask('build:styles', "Builds the stylesheets.",
+                    ['concat:styles', 'copy:styles']);
+                    //['concat:styles', 'cssmin']);
+
+  grunt.registerTask('build', "Builds the application.",
+                    ['build:scripts', 'build:styles']);
+
+  grunt.registerTask('default', 'Builds the application', ['build']);
 }
